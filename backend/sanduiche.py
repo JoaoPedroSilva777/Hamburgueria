f9b3b0ec4578b7c26127ba43bc580043721b5b0f
João Pedro <joaopedrorodriguesdasilva13@gmail.com>
2025-09-17 10:15:20 -0300

    criacao de lanche e cardapio

---
+++ backend/sanduiche.py
@@ -0,0 +1,9 @@
+sanduiches = []
+
+for i in range(4):
+    sanduiche = input("Nome do novo sanduiche:")
+    sanduiche.append(sanduiche)
+
+
+    for sanduiche in sanduiches:
+        print(f"lanche: {sanduiche}")
